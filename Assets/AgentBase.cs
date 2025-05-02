f7b6148c705a54fbf4658a8ac2a5fab3d0728c76
scarsaav <scar.saavedrar@gmail.com>
2025-05-02 10:54:13 -0400

    ajout animation IA unity package

---
+++ Assets/AgentBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentBase : MonoBehaviour
+{
+    [SerializeField] private GameObject _cible;
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(_cible.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
